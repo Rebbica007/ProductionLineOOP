efea63726ca7dbefe6a129939d667a3a0a74f7d3
Rebbica007 <jrgrunewald5184@eagle.fgcu.edu>
2019-09-24 13:59:59 -0400

    res folder commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,24 @@
 package sample;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+  public Button AddProduct;
+  public Button AddRecord;
+
+  @FXML
+  protected void handleAddButtonAction(ActionEvent event) {
+    // Button was clicked, print this
+    System.out.println("Product added.");
+  }
+
+  @FXML
+  protected void handleAddRecordAction(ActionEvent event) {
+    // Button was clicked, print this
+    System.out.println("Record added.");
+  }
+
 }
