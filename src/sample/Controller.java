9f142ab21ebddc96c29f45eec1e8c9ca265efc64
Rebbica007 <jrgrunewald5184@eagle.fgcu.edu>
2019-09-25 11:08:53 -0400

    Product populates in the database

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,72 +1,61 @@
 package sample;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 
 public class Controller {
-  public Button AddProduct;
-  public Button AddRecord;
-  private ComboBox<String> cboTableName = new ComboBox<>();
+  @FXML public Button AddProduct;
+  @FXML private ComboBox<Integer> ProduceCombo;
+  @FXML public Button AddRecord;
 
 
-  @FXML
-  protected void handleAddButtonAction(ActionEvent event) {
-    // Button was clicked, print this
+  public void handleAddButtonAction() {
     System.out.println("Product added.");
-
   }
 
-  @FXML
-  protected void handleAddRecordAction(ActionEvent event) {
-    // Button was clicked, print this
+  public void handleAddRecordAction() {
     System.out.println("Record added.");
-    addToDatabase();
-
   }
 
-  private void addToDatabase() {
+  public void initialize() {
 
     final String JDBC_DRIVER = "org.h2.Driver";
-    final String DB_URL = "jdbc:h2:./res/HR";
+    final String DB_URL = "jdbc:h2:./res/ProductionLine";
 
     //  Database credentials
-    final String USER = "";
-    final String PASS = "";
     Connection conn;
     Statement stmt;
-
-    cboTableName.getItems().add("PRODUCT");
 
     try {
       // STEP 1: Register JDBC driver
       Class.forName(JDBC_DRIVER);
 
-      //STEP 2: Open a connection
+      // STEP 2: Open a connection
       conn = DriverManager.getConnection(DB_URL);
 
-      //STEP 3: Execute a query
+      // STEP 3: Execute a query
       stmt = conn.createStatement();
-
-      String sql = "INSERT INTO PRODUCT(NAME, TYPE, MANUFACTURER) VALUES ('iPod', 'AUDIO', 'Apple') " ;
-
+      String sql = "INSERT INTO PRODUCT(NAME,TYPE,MANUFACTURER) VALUES('iPod','AUDIO','Apple')";
       stmt.executeUpdate(sql);
 
       // STEP 4: Clean-up environment
       stmt.close();
       conn.close();
+
     } catch (ClassNotFoundException | SQLException e) {
       e.printStackTrace();
-
     }
 
+    for (int i = 0; i <= 10; i++) {
+      ProduceCombo.getItems().add(i);
+    }
+
+    ProduceCombo.getSelectionModel().selectFirst();
+    ProduceCombo.setEditable(true);
   }
-
-
 }
